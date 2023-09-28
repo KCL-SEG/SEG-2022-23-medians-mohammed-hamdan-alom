9a41098fb32cab05c1b69745f6a08205f4ff66a6
Mohammed Alom <mohammed.1.alom@kcl.ac.uk>
2023-09-28 14:50:56 +0100

    Attempt 1

--- median.py
+++ median.py
@@ -1,9 +1,16 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(list1):
 
+    list1.sort()
 
+    n = len(list1)
 
+    if n % 2 != 0:
+        return list1[n // 2]
+    else:
+        return (list1[n // 2] + list[n // 2 - 1]) / 2
 
 while True:
     try:
